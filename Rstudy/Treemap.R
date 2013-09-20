edb8879b755e2ebd25ecbcb2891a33e132c48916
xingyemy <ygf199406@gmail.com>
2013-09-20 11:31:51 +0800

    first upload

---
+++ Rstudy/Treemap.R
@@ -0,0 +1,11 @@
+library(treemap)
+data <- read.csv('d:/olympic.csv',T)
+treemap(data,
+        index=c("sports", "events"),
+        vSize="gold",
+        vColor="china",
+        type="value",
+        title='中国奥运金牌分布',
+        fontsize.labels=13,
+        lowerbound.cex.labels=0.7,
+        palette="-RdGy")
